ca77016d0cea3d4eacaf1c03b1a456290a585bd5
zeetasi <ijalxiaomi65@gmail.com>
2025-07-17 19:39:12 +0700

    fix: explicitly set views path for vercel

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const path = require('path');
 const db = require('./db');
@@ -10,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
